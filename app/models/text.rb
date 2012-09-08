d4b309bb041271d2c2be86bfb044d6af7fe84759
Ankit Aggarwal <ankitaggarwal@gmail.com>
2012-09-08 16:49:42 -0400

    Updated text MC

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,5 +1,26 @@
 class Text < ActiveRecord::Base
-    belongs_to :user
+  belongs_to :user
 
-    attr_accessible :userid, :content
+  attr_accessible :userid, :content, :phone_num
+
+  @cfg = YAML.load_file("./config/initializers/api.yml")['twilio']
+
+  @client = Twilio::REST::Client.new(@cfg['account_sid'], @cfg['auth_token'])
+  @account = @client.account
+
+  def new
+    # user = User.find(text.userid)
+
+    puts 'Testing testing testing '
+    twilio = @account.sms.messages.create({
+      :from => @cfg['from_phone'],
+      :to => :phone_num,
+      # :to => '7328581134',
+      # :to => User.find(message.userid.phone_num),
+      :body => :content
+    })
+
+    puts twilio
+  end
+
 end
