24c4d5ead16d2d21d3e6b71aa342c58cbf94221a
Shaan Batra <sb1752@gmail.com>
2012-09-08 15:17:57 -0400

    capture client

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def fitbit
     if params[:oauth_token]
+      client = Fitgem::Client.new(:consumer_key => "608c357377db4e4c8d30dfcdcd7d3621", :consumer_secret => "4e4c5e6798d04fa783ee63c165432484")
       access_token = client.authorize(session['fitbit_request_token'], session['fitbit_request_secret'], { :oauth_verifier => params[:oauth_verifier] })
       current_user.fitbit_token = access_token.token
       current_user.fitbit_secret = access_token.secret
