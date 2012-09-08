a7b33bf1b1a32a43fdecccb27b725ba8361f3d46
Shaan Batra <sb1752@gmail.com>
2012-09-08 12:15:04 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitBitch::Application.config.secret_token = 'e02982e079f3c3074f7e064b90302d66926f9a5ee2564433499bc0880901265a32000cbfd95e9a7cbd2b0a52eaa3c9c233e8145ec7a5f6e368a4ed65a4fd7746'
