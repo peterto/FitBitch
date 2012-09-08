f1bc97360d44ca12200b4f4dac08dcfd26332059
Shaan Batra <sb1752@gmail.com>
2012-09-08 14:41:03 -0400

    fitbit_connect path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
+  def fitbit
 
+  end
+
+  def fitbit_connect
+    #client = Fitgem::Client.new(:consumer_key => "608c357377db4e4c8d30dfcdcd7d3621", :consumer_secret => "4e4c5e6798d04fa783ee63c165432484")
+    render :fitbit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 FitBitch::Application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   # match "registrations/fitbit" => "registrations#fitbit"
-	match "users/fitbit" => "users#fitbit"
-
+	get "users/fitbit" => "users#fitbit"
+  post "users/fitbit_connect" => "users#fitbit_connect"
 
   root :to => "home#index"
 end
