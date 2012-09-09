af75e4996b7871a21258180ddbb206b217de76f3
Shaan Batra <sb1752@gmail.com>
2012-09-09 13:17:14 -0400

    dashboard is pulling user current steps

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,17 @@ class HomeController < ApplicationController
     if Message.where(:user_id => current_user.id).count > params[:msg_count].to_i
       latest_msg = Message.where(:user_id => current_user.id).last
       if latest_msg.content_id # This is a text message
-        render :status => 200, :json => {type: "text", msg_content: latest_msg.content.message, created_at: latest_msg.created_at.in_time_zone("Eastern Time (US & Canada)").strftime("%Y-%m-%d %I:%M%p")}
+        content_msg = latest_msg.content.message
+        if content_msg["STEPS"]
+          content_msg["STEPS"] = current_user.current_steps.to_s
+        end
+        render :status => 200, :json => {type: "text", msg_content: content_msg, created_at: latest_msg.created_at.in_time_zone("Eastern Time (US & Canada)").strftime("%Y-%m-%d %I:%M%p")}
       else # This is an email meme
-        render :status => 200, :json => {type: "email", subject: latest_msg.meme.subject, meme_image_url: latest_msg.meme.image_url, created_at: latest_msg.created_at.in_time_zone("Eastern Time (US & Canada)").strftime("%Y-%m-%d %I:%M%p")}
+        content_msg = latest_msg.meme.subject
+        if content_msg["STEPS"]
+          content_msg["STEPS"] = current_user.current_steps.to_s
+        end
+        render :status => 200, :json => {type: "email", subject: content_msg, meme_image_url: latest_msg.meme.image_url, created_at: latest_msg.created_at.in_time_zone("Eastern Time (US & Canada)").strftime("%Y-%m-%d %I:%M%p")}
       end
     else
       render :status => 401, :json => {}
