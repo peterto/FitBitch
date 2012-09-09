ac5356ee9c435a96cc01cfbe6303b9fd37da5ef6
Ankit Aggarwal <ankitaggarwal@gmail.com>
2012-09-09 07:27:55 -0400

    Separate send sms/email message controller methods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,14 +7,21 @@ class MessagesController < ApplicationController
     @users = User.all
   end
 
-  def send_one
+  def send_sms
     user = User.find(params[:user])
 
     rand_content_id = rand(Content.count)+1
-    rand_meme_id = rand(Meme.count)+1
 
     Text.new.send_message(user, Content.find(rand_content_id))
-    sleep(3)
+
+    redirect_to new_message_path
+  end
+
+  def send_email
+    user = User.find(params[:user])
+
+    rand_meme_id = rand(Meme.count)+1
+
     Email.new.send_message(user, Meme.find(rand_meme_id))
 
     redirect_to new_message_path
