f4ab580f8dd0a7d9492c3f68568b5f0e92c5854f
Shaan Batra <sb1752@gmail.com>
2012-09-08 21:19:30 -0400

    random count from Content

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,7 @@ class MessagesController < ApplicationController
     User.all.each do |user|
       rand_id = rand(Content.count)+1
       sample_message = Content.find(rand_id).message
-      context_message = sample_message.sub(/KEYWORD/, user.current_steps)
+      context_message = sample_message.sub(/KEYWORD/, user.current_steps.to_s)
 
       Text.new(
           user_id: user.id,
