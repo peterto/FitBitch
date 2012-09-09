c0f08a24c33f4177429fa51949b545a19b32a95c
Ankit Aggarwal <ankitaggarwal@gmail.com>
2012-09-08 22:33:28 -0400

    Updated email model

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,14 @@
 class Email < ActiveRecord::Base
+  include HTTParty
+  belongs_to :user
+
+  attr_accessible :user_id, :content
+
+  def send_all
+    # Send email to everyone in the database
+    request = HTTParty.get('http://fitbitch-api.herokuapp.com/email' +
+      '/to/' + User.find(user_id).phone_number +
+      '/body/' + content
+    )
+  end
 end
