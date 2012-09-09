8174f28ea26c9fb3d64faf9ac586ff6904f76f8c
Ankit Aggarwal <ankitaggarwal@gmail.com>
2012-09-08 22:22:50 -0400

    Updated Twilio model to operate with FitBitch API

--- app/models/text.rb
+++ app/models/text.rb
@@ -10,13 +10,19 @@ class Text < ActiveRecord::Base
 
 
   def send_message
-    twilio = @@account.sms.messages.create({
-      :from => ENV['TWILIO_PHONE_NUMBER'],
-      :to => User.find(user_id).phone_number,
-      :body => content
-    })
 
-    puts twilio
+    to_phone = User.find(user_id).phone_number
+    request = HTTParty.get('http://fitbitch-api.herokuapp.com/text' +
+      '/to/' + User.find(user_id).phone_number +
+      '/body/' + content
+    )
+    # twilio = @@account.sms.messages.create({
+    #   :from => ENV['TWILIO_PHONE_NUMBER'],
+    #   :to => User.find(user_id).phone_number,
+    #   :body => content
+    # })
+
+    # puts twilio
   end
 
 end
